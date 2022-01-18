bb70e517b07925b8242e73abf0a46b0819283285
rajeh <dharmateja481@gmail.com>
2022-01-18 23:43:11 +0530

    first

---
+++ script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "To check the disk space on the device"
+echo "=============================================================="
+df -h
+echo "==============================================================="
+echo "end of disc script execuation"
+
+
+echo "check the free space in the machine"
+
+free -m 
+
+ech "end of free script execuation"
